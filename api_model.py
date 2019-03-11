e9be638cc50b925fdeba900649536122a55f68ae
Rounak Vyas <itsron143@gmail.com>
2019-03-11 17:03:51 +0530

    Backup Model Found

---
+++ api_model.py
@@ -0,0 +1,27 @@
+from openalpr import Alpr
+import sys
+
+# Backup Model
+alpr = Alpr("in", "openalpr.conf", "runtime_data")
+if not alpr.is_loaded():
+    print("Error loading OpenALPR")
+    sys.exit(1)
+
+alpr.set_top_n(20)
+alpr.set_default_region("md")
+
+results = alpr.recognize_file("plates.jpg")
+
+i = 0
+for plate in results['results']:
+    i += 1
+    print("Plate #%d" % i)
+    print("   %12s %12s" % ("Plate", "Confidence"))
+    for candidate in plate['candidates']:
+        prefix = "-"
+        if candidate['matches_template']:
+            prefix = "*"
+
+        print("  %s %12s%12f" % (prefix, candidate['plate'], candidate['confidence']))
+
+alpr.unload()
